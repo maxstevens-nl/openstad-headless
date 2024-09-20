5460a5671399b62aecf32524f4a0d4b93585fb98
Max Stevens <maxstevens2708@gmail.com>
2024-09-20 12:40:10 +0200

    remove symlink'ed import

--- apps/api-server/src/middleware/sequelize-authorization-middleware.js
+++ apps/api-server/src/middleware/sequelize-authorization-middleware.js
@@ -1,1 +1,6 @@
-../lib/sequelize-authorization/middleware/index.js
+module.exports = {
+  can: require('../lib/sequelize-authorization/middleware/can'),
+  toAuthorizedJSON: require('../lib/sequelize-authorization/middleware/to-authorized-json'),
+  useReqUser: require('../lib/sequelize-authorization/middleware/use-req-user'),
+  hasRole: require('../lib/sequelize-authorization/lib/hasRole'),
+};
