d1d41ec8c6991dfa6bfb67ce319cf305fef033d3
Max Stevens <maxstevens2708@gmail.com>
2024-09-21 11:30:54 +0200

    logging private create creation process

--- apps/auth-server/scripts/create-privatekey.js
+++ apps/auth-server/scripts/create-privatekey.js
@@ -8,13 +8,22 @@ async function createCert() {
     const certsPath = path.join(__dirname, '../certs');
 
     const privateKeyPath = path.join(certsPath, 'privatekey.pem');
-    await fs.readFile(privateKeyPath).catch(() => exec(`openssl genrsa -out ${privateKeyPath} 2048`));
+    await fs.readFile(privateKeyPath).catch(() => {
+        console.log("Generating private key");
+        return exec(`openssl genrsa -out ${privateKeyPath} 2048`);
+    });
 
     const csrPath = path.join(certsPath, 'certrequest.csr');
-    await fs.readFile(csrPath).catch(() => exec(`openssl req -new -key ${privateKeyPath} -out ${csrPath} -subj '/C=NL/ST=NA/L=NA/O=OpenStad/OU=OpenStad/CN=openstad.${process.env.AUTH_DOMAIN}'`));
+    await fs.readFile(csrPath).catch(() => {
+        console.log("Generating CSR");
+        return exec(`openssl req -new -key ${privateKeyPath} -out ${csrPath} -subj '/C=NL/ST=NA/L=NA/O=OpenStad/OU=OpenStad/CN=openstad.${process.env.AUTH_DOMAIN}'`)
+    });
 
     const publicKeyPath = path.join(certsPath, 'certificate.pem');
-    await fs.readFile(publicKeyPath).catch(() => exec(`openssl x509 -req -in ${csrPath} -signkey ${privateKeyPath} -out ${publicKeyPath}`))
+    await fs.readFile(publicKeyPath).catch(() => {
+        console.log("Generating public key");
+        return exec(`openssl x509 -req -in ${csrPath} -signkey ${privateKeyPath} -out ${publicKeyPath}`)
+    })
 }
 
 createCert()
