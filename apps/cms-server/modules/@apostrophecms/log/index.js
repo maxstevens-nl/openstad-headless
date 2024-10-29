95279b1df333442783030913789d96ffa8fdd63d
Max Stevens <maxstevens2708@gmail.com>
2024-10-29 13:21:45 +0100

    debug: fix logger

--- apps/cms-server/modules/@apostrophecms/log/index.js
+++ apps/cms-server/modules/@apostrophecms/log/index.js
@@ -1,7 +1,5 @@
 module.exports = {
   options: {
-    logger: 'pino',
-    messageAs: 'msg',
     filter: {
       // By module name, or *. We can specify any mix of severity levels and specific event types,
       // and entries are kept if *either* criterion is met
