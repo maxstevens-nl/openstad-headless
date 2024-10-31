a0995ae7da0c12770b312ed0b7b7bde8589b02d2
Max Stevens <maxstevens2708@gmail.com>
2024-10-31 13:32:23 +0100

    fix: update symlink script

--- apps/cms-server/start.sh
+++ apps/cms-server/start.sh
@@ -1,5 +1,5 @@
-ln -s ${APOS_ROOT_DIR}/public $(pwd)/public
-ln -s ${APOS_ROOT_DIR}/data $(pwd)/data
+ln -s /data/public /app/apps/cms-server/public
+ln -s /data/data /app/apps/cms-server/data
 
 node app @apostrophecms/migration:migrate
 
